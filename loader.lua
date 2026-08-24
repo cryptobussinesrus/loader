11ab322095b8f06e2be4b1fda384659b45e9e04d
cryptobussinesrus <shaman8lvl@mail.ru>
2026-08-24 17:37:22 +0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -18,6 +18,7 @@ local games = {
     { Name = "+1 Speed Monkey Escape", File = "+1-Speed-Monkey-Escape.lua", Places = { 114697347887839 } },
     { Name = "+1 Cut Grass Adventure", File = "+1-Cut-Grass-Adventure.lua", Places = { 90086669327265 } },
     { Name = "+1 Web Swing Escape", File = "+1-Web-Swing-Escape.lua", Places = { 110668201954727 } },
+    { Name = "+1 Speed Squirrel Escape", File = "+1-Speed-Squirrel-Escape.lua", Places = { 73613510857111 } },
     { Name = "Throw a Coin", File = "Throw-a-Coin.lua", Places = { 115681808123944, 72042130041700, 100875131717601 } },
 }
 
